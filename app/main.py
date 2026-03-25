3b5be5e942de268b94afd17cde33c9ab53e63c11
Shine057 <shadow9999900000@gmail.com>
2026-03-25 23:45:34 +0300

    lab1 the work program

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI
+﻿from fastapi import FastAPI
 from datetime import date
 
 app = FastAPI()
 
 @app.get("/info")
 async def get_info():
-    """
-    Возвращает количество дней до наступления Нового года.
-    """
     today = date.today()
     next_new_year = date(today.year + 1, 1, 1)
     days_left = (next_new_year - today).days
